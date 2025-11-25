e60334a358803a6876606ad34432e7ab36115ca6
Jonathan Tindungan <89713311+Nathan-Front@users.noreply.github.com>
2025-11-25 22:10:11 +0900

    Added some more design

--- script/order.js
+++ script/order.js
@@ -88,7 +88,7 @@ function itemFromStorageOnReload(){
     //Loop the array of sessionStorage 
     cartStorage.forEach((item, index) =>{
         const li = document.createElement('li');
-        li.className = 'cart-content';
+        li.className = 'cart-content-item';
        // li.setAttribute('data-item-id', `${item.StoredId}`);
         li.id = 'for-update-cart';
         li.innerHTML = `
@@ -96,7 +96,7 @@ function itemFromStorageOnReload(){
                         <p>Item name: ${item.name}</p>
                         <p>Item price $<span id="item-price">9.75</span></p>
                         <p>seller: ABC comp.</p>
-                        <img class="item-img" src="${item.image}" alt="Product image">
+                        <img src="${item.image}" alt="Product image">
                         <h2>Size:<span class="selected-size">${item.size}</span></h2>
                         <span>Quantity; ${item.quantity}</span>
                         <span>Total: $${item.total}</span>                       
@@ -233,7 +233,7 @@ function updateCartContent(itemId){
     cartStore.forEach((item, index) =>{
         const itemQty = document.getElementsByClassName(".item-count");
         const li = document.createElement('li');
-        li.className = 'cart-content';
+        li.className = 'cart-content-item';
       //  li.setAttribute('data-item-id', `${item.StoredId}`);
         li.id = 'for-update-cart';
         li.innerHTML = `
@@ -241,7 +241,7 @@ function updateCartContent(itemId){
                         <p>Item name: ${item.name}</p>
                         <p>Item price $<span id="item-price">9.75</span></p>
                         <p>seller: ABC comp.</p>
-                        <img class="item-img" src="${item.image}" alt="Product image">
+                        <img src="${item.image}" alt="Product image">
                         <h2>Size:<span class="selected-size">${item.size}</span></h2>
                         <span>Quantity; ${item.quantity}</span>
                         <span>Total: $${item.total}</span>                       
@@ -291,32 +291,66 @@ document.addEventListener('DOMContentLoaded', () =>{
     if(filterBtn){
         filterBtn.addEventListener('click', filterFunction);
     }
+    const filterCancelBtn = document.getElementById('filter-button-cancel');
+    filterCancelBtn.addEventListener('click', function (){
+        location.reload();
+    });
+    //For the cart content
+    //Get the cart button when clicked
     const checkoutCartContent = document.getElementById('check-out-cart');
+    //Get the Id of container aside. This is for cart display
     const cartContentEmpty = document.getElementById('display-cart');
+    //This will cover the page when cart is being dispayed. Prevent access to the main page
     const coverPage = document.getElementById('overlay');
     checkoutCartContent.addEventListener('click', function(){
+    //Display the cart    
     if(cartContentEmpty.style.display !== 'none'){
+        //Add this class name to the container as if it's in display:none
         cartContentEmpty.classList.add('navActive');
+        //This prevents access to the main page while cart is on display
         coverPage.style.display = 'block';
     }else{
         cartContentEmpty.classList.remove('navActive');
         coverPage.style.display = 'none';
     }
     });
-    
+    //When close cart button was clicked same with else condition above
     const closeCart = document.getElementById('close-cart');
     closeCart.addEventListener('click', function(){
     if(closeCart){
         cartContentEmpty.classList.remove('navActive');
         coverPage.style.display = 'none';
+        //Added this to reload the page to be able to add same item if it was deleted from the cart
+        //Basically reloads the function of adding item to the cart 
         location.reload();
     }
     });  
 });
 
 
-
- 
+window.onload = function(){
+    //Get all buttons
+    const newsBtn = document.querySelectorAll('.new-item-display-button');
+    newsBtn.forEach(function (btnEl){
+        btnEl.addEventListener('click', () => {
+        //Remove the newsItemActive class name and add it to the next clicked button
+        document.querySelector('.newsItemActive').classList.remove('newsItemActive');
+        btnEl.classList.add('newsItemActive');
+        });
+    });
+}
+//USe data- and target the related item with Id name same as the data-target value
+document.querySelectorAll('button[data-target]').forEach(function (el){
+    el.addEventListener('click', () =>{
+        //Get list of the items to be displayed one by one
+        let newsContainer = document.getElementsByClassName('new-item-panel');
+        for(let i = 0; i < newsContainer.length; i++){
+            newsContainer[i].style.display = 'none';
+        }
+        //Target the Id name with the same data-target value of the clicked button
+        document.getElementById(el.getAttribute('data-target')).style.display = "flex";
+    });
+}); 
 
 /*
 let currentIndex = 0;
@@ -361,52 +395,98 @@ carouselContainer.addEventListener('mousemove', (e) => {
     innerCarouselContainer.style.left = `${x - indexStartAt}px`;
 });*/
 
+//Start of display index
+let upperCarouselIndex = 0;
+//All container with this class name
+const upperSlides = document.querySelectorAll('.upper-carousel');
+//For auto animation slide
+let intervalId = null;
+//Call the function 
+document.addEventListener('DOMContentLoaded', initialSlide); 
+function initialSlide(){
+    if(upperSlides.length > 0){
+        //Add this class name to the current container on display
+        upperSlides[upperCarouselIndex].classList.add('upper-display');
+        //Automatically go to next slide after 10 seconds
+        intervalId = setInterval(nextSlide, 7000);
+    }
+}
+function showSlide(index){
+    //If next button clicked and reached and pass the last slide, return to 1st slide
+    if(index >= upperSlides.length){
+        upperCarouselIndex = 0;
+    //If previsou button clicked and pass 1st slide, display last slide    
+    }else if(index < 0){
+        upperCarouselIndex = upperSlides.length - 1;
+    }
+    //Remove this class name from the slide to diplay the next slide with this class name added into it
+    upperSlides.forEach(slides => {
+        slides.classList.remove('upper-display');
+    });
+    //Finally display the selected slide by adding this class name
+    upperSlides[upperCarouselIndex].classList.add('upper-display');
+}
+//Previous button
+function prevSlide(){
+    //When previous button is clicked the auto aniamtion will stop = null
+    clearInterval(intervalId);
+    upperCarouselIndex--;
+    showSlide(upperCarouselIndex);
+}
+//Next button
+function nextSlide(){
+    upperCarouselIndex++;
+    showSlide(upperCarouselIndex);
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+ //Get the Ul tag containers using its class name. Container for the carousel contents
  const carouselItemContainer = document.querySelector('.owl-carousel');
+ //For previous and next button arrows
  const arrowBtns = document.querySelectorAll('.owl-carousel-container i');
+ //Get the containers of each UL tags 
  const carouselContainer = document.querySelector('.owl-carousel-container');
-
+ //Get LI tag as the carousel contents
  const firstItem = carouselItemContainer.querySelector('.carousel-item');
+ //Get the layout width of the container/carousel item with all its contents
  const firstItemWidth = firstItem.offsetWidth;
-
+ //Declare for when dragging on the screen
  let isDrag = false,
-     startX,
-     startScrollLeft,
-     timeoutId;
+     startX,//Left/right movement
+     startScrollLeft,//Storing scroll movement X axis
+     timeoutId;//Scroll after mouse pointer leaving the container
+ //When clicking and holding mouse
  const dragStart = (e) => {
     isDrag = true;
-    carouselItemContainer.classList.add('dragging');
-    startX = e.pageX;
+    carouselItemContainer.classList.add('dragging');//Add this class name when dragging
+    startX = e.pageX;//Mouse cursor horizontal movement
     startScrollLeft = carouselItemContainer.scrollLeft;
  };
+ //When removing mouse cursor out of the container
  const dragging = (e) => {
     if(!isDrag) return;
-
-    const newScrollToLeft = startScrollLeft -(e.pageX - startX);
-
+    const newScrollToLeft = startScrollLeft -(e.pageX - startX);//Get scroll to left action
+    //If reached end of container to scroll to
     if(newScrollToLeft <= 0 || newScrollToLeft >=
        carouselItemContainer.scrollWidth - carouselItemContainer.offsetWidth ){
         isDrag = false;
         return;
        }
-       carouselItemContainer.scrollLeft = newScrollToLeft;
+       carouselItemContainer.scrollLeft = newScrollToLeft;//Keep scrolling left
  };
- 
+ //If drag is released
  const dragStop = () => {
     isDrag = false;
     carouselItemContainer.classList.remove('dragging');
  };
-
+//When leaving mouse cursor out of the container, scroll to left
  const autoPlay = () => {
-    if(window.innerWidth < 600) return;
-
+    if(window.innerWidth < 900) return; 
     const totalItemWidth = carouselItemContainer.scrollWidth;
     const maxSCrollToLeft = totalItemWidth - carouselItemContainer.offsetWidth;
-
     if(carouselItemContainer.scrollLeft >= maxSCrollToLeft) return;
-
-    timeoutId = setTimeout(() => 
-                carouselItemContainer.scrollLeft += firstItemWidth);
+    timeoutId = setTimeout(() => carouselItemContainer.scrollLeft += firstItemWidth);
  };
   
  carouselItemContainer.addEventListener('mousedown', dragStart);
@@ -415,6 +495,7 @@ document.addEventListener('DOMContentLoaded', function() {
  carouselContainer.addEventListener('mouseenter', () => clearTimeout(timeoutId));
  carouselContainer.addEventListener('mouseleave', autoPlay);
 
+ //Previous/next button
  arrowBtns.forEach(btn => {
     btn.addEventListener('click', () => {
             carouselItemContainer.scrollLeft += btn.id === 'left' ? -firstItemWidth : firstItemWidth;
@@ -428,7 +509,6 @@ document.addEventListener('DOMContentLoaded', function() {
  const carouselItemContainerShoes = document.querySelector('.owl-carousel-shoes');
  const arrowBtns = document.querySelectorAll('.owl-carousel-container-shoes i');
  const carouselContainerShoes = document.querySelector('.owl-carousel-container-shoes');
-
  const firstItem = carouselItemContainerShoes.querySelector('.carousel-item-shoes');
  const firstItemWidth = firstItem.offsetWidth;
 
@@ -444,9 +524,7 @@ document.addEventListener('DOMContentLoaded', function() {
  };
  const dragging = (e) => {
     if(!isDrag) return;
-
     const newScrollToLeft = startScrollLeft -(e.pageX - startX);
-
     if(newScrollToLeft <= 0 || newScrollToLeft >=
        carouselItemContainerShoes.scrollWidth - carouselItemContainerShoes.offsetWidth ){
         isDrag = false;
@@ -454,22 +532,16 @@ document.addEventListener('DOMContentLoaded', function() {
        }
        carouselItemContainerShoes.scrollLeft = newScrollToLeft;
  };
- 
  const dragStop = () => {
     isDrag = false;
     carouselItemContainerShoes.classList.remove('dragging');
  };
-
  const autoPlay = () => {
     if(window.innerWidth < 600) return;
-
     const totalItemWidth = carouselItemContainerShoes.scrollWidth;
     const maxSCrollToLeft = totalItemWidth - carouselItemContainerShoes.offsetWidth;
-
     if(carouselItemContainerShoes.scrollLeft >= maxSCrollToLeft) return;
-
-    timeoutId = setTimeout(() => 
-                carouselItemContainerShoes.scrollLeft += firstItemWidth);
+    timeoutId = setTimeout(() => carouselItemContainerShoes.scrollLeft += firstItemWidth);
  };
   
  carouselItemContainerShoes.addEventListener('mousedown', dragStart);
@@ -610,4 +682,81 @@ function updateActiveDot() {
 }
 
 // Initial active dot setting
-updateActiveDot();*/
+updateActiveDot();*//*
+const throttle = (func, time = 100) => {
+  let lastTime = 0;
+  return () => {
+    const now = new Date();
+    if (now - lastTime >= time) {
+      func();
+      time = now;
+    }
+  };
+};
+//window.addEventListener('scroll', throttle(validateHeader, 100));
+  let lastScrollTop = 0;
+  //const header = document.querySelector('header');
+/*
+const validateHeader = () => {
+    const windowY = window.scrollY;
+    const windowH = window.innerHeight;
+
+    if(windowY > windowH){
+        header.classList.add('is-fixed');
+        if(windowY > windowH + 40){
+            header.classList.add('is-animated');
+            if(windowY < lastScrollTop){
+                header.classList.add('scroll-up');
+            }else{
+                header.classList.remove('scroll-up');
+            }
+        }else{
+        header.classList.remove('scroll-up');
+        }
+    }else{
+        header.classList.remove('is-fixed', 'is-animated');
+    }
+    lastScrollTop = windowY;
+};*/
+
+/*
+  window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+      // Scrolling Down: Hide the header
+      header.classList.remove('scroll-up');
+    } else {
+      // Scrolling Up: Show the header
+      header.classList.add('scroll-up');
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  });
+/*
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var header = document.getElementById("display-on-scroll");
+
+  if (prevScrollpos > currentScrollPos) {
+    // Scrolling up, show the header
+    header.classList.remove("scroll-down-hide");
+  } else {
+    // Scrolling down, hide the header
+    header.classList.add("scroll-down-hide");
+  }
+  prevScrollpos = currentScrollPos;
+}*/
+
+
+var lastScrollTop = 0;
+//Get Title container
+const header =document.getElementById('display-on-scroll');
+window.addEventListener('scroll', function () {
+    var scrollTop = window.screenY || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){//Basically when scrolling up
+        header.style.top ='-10%';//Display the title container
+    }else{
+        header.style.top ='0%';//Hide when scrolling down
+    }
+    lastScrollTop = scrollTop;
+});
